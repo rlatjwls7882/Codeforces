b572b99a03e21871c2524fb8c91a809a5bb0323d
Seojin Kim <rlatjwls7882@gmail.com>
2026-07-17 22:18:38 +0900

    Create D2.cpp

---
+++ 2xxx/2245/D2.cpp
@@ -0,0 +1,101 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/rope>
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("fma")
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+
+#define x first
+#define y second
+#define sz(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+#define rep(x) for(int __i=(x);__i>0;--__i)
+#define compress(x) sort(all(x)), x.erase(unique(all(x)), x.end())
+
+typedef long long ll;
+typedef long double ld;
+typedef __int128 i128;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename T> T sq(T x) { return x*x; }
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const ld PI = acosl(-1);
+const ld EPS = 1e-10;
+
+mt19937 rd((unsigned)chrono::steady_clock::now().time_since_epoch().count());
+uniform_int_distribution<int> rnd_int(0, 0); // rnd_int(rd)
+uniform_real_distribution<double> rnd_real(0, 1); // rnd_real(rd)
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int t; cin >> t;
+    while(t--) {
+        int n, m; cin >> n >> m;
+
+        vi plus(n+1), minu(n+1), res(n+1);
+        vvi plusConn(n+1), minusConn(n+1);
+        while(m--) {
+            int o, i, j; cin >> o >> i >> j;
+            if(o==1) {
+                plusConn[i].push_back(j);
+                plus[i]++;
+                if(i!=j) {
+                    plusConn[j].push_back(i);
+                    plus[j]++;
+                }
+            } else {
+                minusConn[i].push_back(j);
+                minu[i]++;
+                if(i!=j) {
+                    minusConn[j].push_back(i);
+                    minu[j]++;
+                }
+            }
+        }
+
+        queue<int> q;
+        for(int i=1;i<=n;i++) {
+            if(!plus[i] || !minu[i]) {
+                q.push(i);
+            }
+        }
+
+        int cnt=0;
+        while(!q.empty()) {
+            int cur=q.front(); q.pop();
+            if(res[cur]) continue;
+            if(plus[cur]) {
+                res[cur]=n-cnt++;
+                for(auto nxt:plusConn[cur]) {
+                    if(--plus[nxt]==0) {
+                        q.push(nxt);
+                    }
+                }
+            } else {
+                res[cur]=-(n-cnt++);
+                for(auto nxt:minusConn[cur]) {
+                    if(--minu[nxt]==0) {
+                        q.push(nxt);
+                    }
+                }
+            }
+        }
+        if(cnt!=n) {
+            cout << "NO\n";
+        } else {
+            cout << "YES\n";
+            for(int i=1;i<=n;i++) cout << res[i] << ' ';
+            cout << '\n';
+        }
+    }
+}
